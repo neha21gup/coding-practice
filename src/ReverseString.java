ad3059a5dc4415d26cf245bca6182cac7eb86bf0
nehag2 <nehag2@cisco.com>
2020-02-21 23:28:45 +0530

    Adding Reverse String

---
+++ src/ReverseString.java
@@ -0,0 +1,23 @@
+//Reverse a char array
+
+public class ReverseString {
+    public static void main(String[] args){
+        //char[] s = new char[5];
+        char[] s = {'h','e','l','l','o'};
+        System.out.println(s);
+        reverse(s);
+
+    }
+
+    public static void reverse(char[] s){
+        int j = s.length-1;
+        for(int i = 0; i < j; i++){
+            char temp = s[j];
+            s[j] = s[i];
+            s[i] = temp;
+
+            j--;
+        }
+        System.out.println(s);
+    }
+}
